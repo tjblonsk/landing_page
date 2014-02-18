71c018af0bd5750c5898b4c843638a070f791f05
Tim <tim.blonski@gmail.com>
2014-02-17 21:22:48 -0500

    added more styling.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -34,7 +34,9 @@ $(function(){
   /*
    * Homepage image
    */
-  function resizeBannerImage() {
+  window.resizeBannerImage = function() {
+    console.log('resizing')
+    var debug = true;
     var $img = $('.landing_img');
     var windowHeight = $(window).height();
     var windowWidth = $(window).width();
@@ -45,8 +47,18 @@ $(function(){
     var marginleftpx;
     var marginToppx;
 
+
     var imgAspect = imgWidth / imgHeight;
     var windowAspect = windowWidth / windowHeight;
+
+    if (debug){
+      console.log('windowHeight', windowHeight);
+      console.log('windowWidth', windowWidth);
+      console.log('imgHeight', imgHeight);
+      console.log('imgWidth', imgWidth);
+      console.log('imgAspect', imgAspect);
+      console.log('windowAspect', windowAspect);
+    }
 
     if (imgAspect > windowAspect) {
       // img is wider than window
@@ -58,6 +70,7 @@ $(function(){
       marginleft = ($img.width() - windowWidth) / 2;
       marginleftpx = '-' + marginleft + 'px';
       $img.css('margin-left', marginleftpx);
+      $img.css('margin-top', 0);
     }
     else if (imgAspect < windowAspect) {
       // window is wider than img
@@ -69,18 +82,21 @@ $(function(){
       marginTop = ($img.height() - windowHeight) / 2;
       marginToppx = '-' + marginTop + 'px';
       $img.css('margin-top', marginToppx);
+      $img.css('margin-left', 0);
     }
     else {
       // window and image are the same size
       $img.css({
         'height': windowHeight,
-        'width': windowWidth
+        'width': windowWidth,
+        'margin': 0
       });
     }
   }
 
   resizeBannerImage();
   $(window).resize(throttle(resizeBannerImage, 10));
+  window.addEventListener('orientationchange', resizeBannerImage);
   $('.landing_img').imagesLoaded(function() {
     resizeBannerImage();
   });
